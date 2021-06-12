0be4a21c6cb05bfb30334d1e15db7a5e241ede00
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2021-06-12 12:00:03 +0300

    fsdf

---
+++ keyBoard.py
@@ -0,0 +1,6 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+
+butonTK=KeyboardButton("Кафедра технической кибернетики")
+
+greetKB=ReplyKeyboardMarkup()
+greetKB.add(butonTK)
