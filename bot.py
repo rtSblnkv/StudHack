b4b89f582fb1df54b7e3d3c92087267e17afc134
rtSblnkv <artsab001@gmail.com>
2021-06-12 13:45:18 +0400

    db integration

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 from aiogram import Bot,Dispatcher,executor,types
 import logging
+import config
+import sqlite3
+
+
 
 commands_ ={
     'start': 'Привет,студент! Этот бот поможет тебе найти научрука!',
@@ -7,15 +11,8 @@ commands_ ={
 }
 
 
-
-TOKEN = '1760229546:AAEHaPDUy7YBkHIfMvV2_8gTzp6pOPD1QrU'
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
-
-
-user = dp.get_me()
-
-print(user)
 
 @dp.message_handler(commands=['start'])
 async def send_hello_answer(message:types.Message):
@@ -27,5 +24,5 @@ async def send_help_titles(message:types.Message):
     msg = text(bold('Доступные команды'),commands_.keys(),sep=('\n'))
     await message.answer(msg)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     executor.start_polling(dp,skip_updates=True)
